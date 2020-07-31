f74f8200789c1f4dde4312547c31642723f16f44
김 동규 <rebob9545@gmail.com>
2020-07-31 20:06:26 +0900

    플레이어 뛰는 모션 추가

--- Space/Player.cpp
+++ Space/Player.cpp
@@ -3,27 +3,93 @@
 
 Player::Player()
 {
-	m_Sprite = Sprite::Create(L"Painting/Player/Attack/1.png");
-	m_Sprite->SetParent(this);
+	m_Idle = new Animation();
+	m_Idle->SetParent(this);
+	m_Idle->Init(7, true);
+	m_Idle->AddContinueFrame(L"Painting/Player/Idle/", 1, 11);
+
+	m_RRun = new Animation();
+	m_RRun->SetParent(this);
+	m_RRun->Init(7, true);
+	m_RRun->AddContinueFrame(L"Painting/Player/RRun/", 1, 8);
+
+	m_LRun = new Animation();
+	m_LRun->SetParent(this);
+	m_LRun->Init(7, true);
+	m_LRun->AddContinueFrame(L"Painting/Player/LRun/", 1, 8);
+
+	m_Player = m_Idle;
+	m_Player->SetParent(this);
+	m_PlayerStatus = Status::IDLE;
+
 	SetPosition(1920 / 2, 1080 / 2);
 
 	m_Line = new LineMgr();
 	m_Line->Init(1, true);
 	m_Line->SetColor(D3DXCOLOR(255, 255, 255, 255));
+
+	m_Speed = 500.f;
+
+	m_BaseAngle = 0;
+	m_LandAngle = 0 + m_BaseAngle;
+
+	m_BaseAngle = 90;
 }
 
 Player::~Player()
 {
 }
 
+void Player::Run()
+{
+	if (INPUT->GetKey(VK_RIGHT) == KeyState::PRESS)
+	{
+		m_Player = m_RRun;
+		Translate(m_Directon.x * m_Speed * dt, m_Directon.y * -m_Speed * dt);
+		m_PlayerStatus = Status::RUN;
+	}
+	if (INPUT->GetKey(VK_RIGHT) == KeyState::UP)
+	{
+		m_Player = m_Idle;
+		m_PlayerStatus = Status::IDLE;
+	}
+
+	if (INPUT->GetKey(VK_LEFT) == KeyState::PRESS)
+	{
+		m_Player = m_LRun;
+		Translate(m_Directon.x * -m_Speed * dt, m_Directon.y * m_Speed * dt);
+		m_PlayerStatus = Status::RUN;
+	}
+	if (INPUT->GetKey(VK_LEFT) == KeyState::UP)
+	{
+		m_Player = m_Idle;
+		m_PlayerStatus = Status::IDLE;
+	}
+}
+
+void Player::SetDirection()
+{
+	Vec2 rot;
+	rot.x = cos(D3DXToRadian((float)m_LandAngle));
+	rot.y = sin(D3DXToRadian((float)m_LandAngle));
+
+	m_Directon = m_Position - rot;
+
+	D3DXVec2Normalize(&m_Directon, &rot);
+}
+
 void Player::Update(float deltaTime, float Time)
 {
+	SetDirection();
+	Run();
+
 	SetVertex();
+	m_Player->Update(deltaTime,Time);
 }
 
 void Player::Render()
 {
-	m_Sprite->Render();
+	m_Player->Render();
 	m_Line->DrawLine(m_Vertex, 5);
 }
 
--- Space/Player.h
+++ Space/Player.h
@@ -1,12 +1,31 @@
 #pragma once
+enum class Status
+{
+	IDLE,
+	RUN,
+	NONE
+};
 class Player : public Object
 {
-	Sprite* m_Sprite;
+	Animation* m_Idle;
+	Animation* m_RRun;
+	Animation* m_LRun;
+
+	Animation* m_Player;
 
 	LineMgr* m_Line;
 public:
 	Player();
 	~Player();
+
+	Vec2 m_Directon;
+	int m_LandAngle;
+	int m_BaseAngle;
+	float m_Speed;
+	Status m_PlayerStatus;
+
+	void Run();
+	void SetDirection();
 
 	void Update(float deltaTime, float Time);
 	void Render();
