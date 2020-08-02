606c07ccea2541bf2524c6d5ac7e22eae149d36b
김 동규 <rebob9545@gmail.com>
2020-08-03 04:04:21 +0900

    이미지 수정

--- Space/Castle.cpp
+++ Space/Castle.cpp
@@ -19,8 +19,8 @@ void Castle::Init()
 
 	UI::GetInst()->Init();
 
-	MapLimit::GetInst()->CreateMap(HORIZONTAL, 25, Vec2(-1920 / 2, m_BG->m_Size.y - 50));
 	MapLimit::GetInst()->CreateMap(VERTICAL, 20, Vec2(-1920 / 2, m_BG->m_Size.y + 300));
+	MapLimit::GetInst()->CreateMap(HORIZONTAL, 25, Vec2(-1920 / 2, m_BG->m_Size.y - 50));
 }
 
 void Castle::Release()
