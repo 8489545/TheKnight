d8a84bf44ceaf461c132c525737117a7f64ef36c
8489545 <rebob9545@gmail.com>
2020-08-10 11:36:48 +0900

    monster1 충돌박스 구현

--- Space/Monster1.cpp
+++ Space/Monster1.cpp
@@ -14,7 +14,13 @@ Monster1::Monster1(Vec2 Pos)
 	m_Idle->SetParent(this);
 
 	m_Monster = m_Idle;
+	m_Status = Status::IDLE;
 	m_Monster->SetParent(this);
+
+	m_ColBox = Sprite::Create(L"Painting/Monster/Monster1/ColBox.png");
+	m_RightBox = Sprite::Create(L"Painting/Monster/Monster1/Side.png");
+	m_LeftBox = Sprite::Create(L"Painting/Monster/Monster1/Side.png");
+	m_Foot = Sprite::Create(L"Painting/Monster/Monster1/Foot.png");
 
 	m_Position = Pos;
 
@@ -31,6 +37,11 @@ Monster1::~Monster1()
 
 void Monster1::Update(float deltaTime, float Time)
 {
+	m_ColBox->SetPosition(m_Position.x, (m_Position.y + m_Size.y / 2) - m_ColBox->m_Size.y/ 2);
+	m_RightBox->SetPosition(m_Position.x + m_ColBox->m_Size.x / 2, m_ColBox->m_Position.y);
+	m_LeftBox->SetPosition(m_Position.x - m_ColBox->m_Size.x / 2, m_ColBox->m_Position.y);
+	m_Foot->SetPosition(m_Position.x, (m_ColBox->m_Position.y + m_ColBox->m_Size.y / 2) - m_Foot->m_Size.y / 2);
+
 	SetVertex();
 	m_Monster->Update(deltaTime, Time);
 }
@@ -39,6 +50,11 @@ void Monster1::Render()
 {
 	m_Line->DrawLine(m_Vertex, 5);
 	m_Monster->Render();
+
+	m_ColBox->Render();
+	m_RightBox->Render();
+	m_LeftBox->Render();
+	m_Foot->Render();
 }
 
 void Monster1::OnCollision(Object* other)
--- Space/Monster1.h
+++ Space/Monster1.h
@@ -5,10 +5,17 @@ class Monster1 : public Object
 	Animation* m_Idle;
 	Animation* m_Monster;
 
+	Sprite* m_ColBox;
+	Sprite* m_RightBox;
+	Sprite* m_LeftBox;
+	Sprite* m_Foot;
+
 	LineMgr* m_Line;
 public:
 	Monster1(Vec2 Pos);
 	~Monster1();
+
+	Status m_Status;
 
 	void Update(float deltaTime, float Time);
 	void Render();
